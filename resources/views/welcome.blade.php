a8cda7255c1d45b957786883d66d7d783d51fc08
Sander Geraedts <sandergeraedts@live.nl>
2017-01-02 19:37:55 +0100

    signup working and got dev env setup. Time to start cranking!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,40 @@
 @endsection
 
 @section('body')
-    <h1>Hour of Code</h1>
+    <div class="row">
+        <div class="col-md-4">
+            <form action="{{ route('signup') }}" method="post">
+                <h3>Sign up</h3>
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="name">Your Name</label>
+                    <input class="form-control" type="text" name="name" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="hidden" name="_token" value="{{ Session::token() }}">
+            </form>
+        </div>
+        <div class="col-md-2"></div>
+        <div class="col-md-4">
+            <form action="#" method="post">
+                <h3>Sign in</h3>
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
 @endsection
